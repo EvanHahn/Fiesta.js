216b95026147176bc10269bcabe9cca17befa2ae
EvanHahn <me@evanhahn.com>
2011-06-14 16:42:41 -0700

    Unit tests: Browser API
    3 unit tests: getBrowser(), getBrowserVersion(), getOS(). Sadly, these
    are rather difficult to test without copy-pasting their functionality.

--- tests/spec/browser_spec.js
+++ tests/spec/browser_spec.js
@@ -1,2 +1,17 @@
 describe("Browser API", function() {
+	
+	it("tells me what browser I'm using", function() {
+		var possibilities = ["Firefox", "Chrome", "Safari", "Opera", "Explorer", "Opera", "RockMelt", "OmniWeb", "iCab", "KDE", "Camino", "Netscape"];
+		expect(possibilities).toContain(Fiesta.getBrowser());
+	});
+	
+	it("tells me the browser's version", function() {
+		expect(typeof Fiesta.getBrowserVersion()).toEqual(typeof 1);
+	});
+	
+	it("tells me what OS I'm using", function() {
+		var possibilities = ["Windows", "Mac", "iPhone/iPod", "Linux"];
+		expect(possibilities).toContain(Fiesta.getOS());
+	});
+	
 });
