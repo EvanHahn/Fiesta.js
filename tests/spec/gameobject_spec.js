0365bf729ef79b68fcd464b9788268fce56910b6
EvanHahn <me@evanhahn.com>
2011-06-14 17:13:39 -0700

    Unit test: Game object
    This is pretty thorough, so far. Here are the current unit tests:
    
    * Initializes with no graphic
    * Initializes with no playground
    * Assigns graphics
    * Throws an error for bad graphic assignment
    * Spawns in a playground
    * Can be destroyed
    * onSpawn
    * onDestroy
    * onFrames

--- tests/spec/gameobject_spec.js
+++ tests/spec/gameobject_spec.js
@@ -1,2 +1,104 @@
 describe("Game object", function() {
+	
+	it("initializes with no graphic", function() {
+		var obj = new Fiesta.GameObject();
+		expect(obj.getGraphic()).toBeFalsy();
+	});
+	
+	it("initializes with no playground", function() {
+		var obj = new Fiesta.GameObject();
+		expect(obj.getPlayground()).toBeFalsy();
+	});
+	
+	it("assigns graphics properly", function() {
+		var obj = new Fiesta.GameObject();
+		var gfx = new Fiesta.Graphic();
+		obj.setGraphic(gfx);
+		expect(obj.getGraphic()).toBe(gfx);
+	});
+	
+	it("won't assign a graphic if you don't pass it a graphic", function() {
+		var obj = new Fiesta.GameObject();
+		expect(function() {
+			obj.setGraphic("foo");
+		}).toThrow("foo is not a graphic");
+	});
+
+	it("spawns in a playground", function() {
+		var obj = new Fiesta.GameObject();
+		var play = new Fiesta.Playground();
+		play.spawn(obj);
+		expect(obj.getPlayground()).toBe(play);
+	});
+	
+	it("destroys itself from a playground", function() {
+		var obj = new Fiesta.GameObject();
+		var play = new Fiesta.Playground();
+		play.spawn(obj);
+		expect(obj.getPlayground()).toBe(play);
+		obj.destroy();
+		expect(obj.getPlayground()).toBeFalsy();
+	});
+	
+	it("calls onSpawn when you spawn an object", function() {
+		var Obj = new Fiesta.Class(Fiesta.GameObject, {
+			initialize: function() {
+				this.callSuper();
+				this.spawned = false;
+			},
+			onSpawn: function() {
+				this.callSuper();
+				this.spawned = true;
+			}
+		});
+		var obj = new Obj();
+		expect(obj.spawned).toBeFalsy();
+		var play = new Fiesta.Playground();
+		play.spawn(obj);
+		expect(obj.spawned).toBeTruthy();
+	});
+	
+	it("calls onDestroy when you destroy an object", function() {
+		var Obj = new Fiesta.Class(Fiesta.GameObject, {
+			initialize: function() {
+				this.callSuper();
+				this.destroyed = false;
+			},
+			onDestroy: function() {
+				this.callSuper();
+				this.destroyed = true;
+			}
+		});
+		var obj = new Obj();
+		expect(obj.destroyed).toBeFalsy();
+		var play = new Fiesta.Playground();
+		play.spawn(obj);
+		obj.destroy();
+		expect(obj.destroyed).toBeTruthy();
+	});
+	
+	it("calls onFrame every frame", function() {
+		var Obj = new Fiesta.Class(Fiesta.GameObject, {
+			initialize: function() {
+				this.callSuper();
+				this.value = 0;
+			},
+			onFrame: function() {
+				this.callSuper();
+				this.value ++;
+			}
+		});
+		var obj = new Obj();
+		expect(obj.value).toEqual(0);
+		var play = new Fiesta.Playground(1, 1, "2d", 10);
+		play.spawn(obj);
+		expect(obj.value).toEqual(0);
+		play.place(document.body);
+		waits(500);
+		runs(function() {
+			expect(obj.value).toBeGreaterThan(5);
+			expect(obj.value).toBeLessThan(10);
+		});
+	});
+	
 });
