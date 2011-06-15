62327b95fc2080ab36b6a9a09d4aa2d2097df216
EvanHahn <me@evanhahn.com>
2011-06-15 11:17:53 -0700

    Unit tests: Math API, Fiesta.sign

--- tests/spec/math_spec.js
+++ tests/spec/math_spec.js
@@ -1,2 +1,30 @@
 describe("Math API", function() {
+	
+	it("tells me a number is negative when it's negative", function() {
+		expect(Fiesta.sign(-10)).toEqual(-1);
+		expect(Fiesta.sign(-1)).toEqual(-1);
+		expect(Fiesta.sign(-Infinity)).toEqual(-1);
+		expect(Fiesta.sign(-0.000000000000001)).toEqual(-1);
+	});
+	
+	it("tells me a number is positive when it's positive", function() {
+		expect(Fiesta.sign(10)).toEqual(1);
+		expect(Fiesta.sign(1)).toEqual(1);
+		expect(Fiesta.sign(Infinity)).toEqual(1);
+		expect(Fiesta.sign(0.000000000000001)).toEqual(1);
+	});
+	
+	it("tells me a number is 0 when it's 0", function() {
+		expect(Fiesta.sign(0)).toEqual(0);
+	});
+	
+	it("throws an error if you ask for the sign of a non-number", function() {
+		expect(function() {
+			Fiesta.sign(false);
+		}).toThrow("Cannot find sign of boolean false");
+		expect(function() {
+			Fiesta.sign("10");
+		}).toThrow("Cannot find sign of string 10");
+	});
+	
 });
