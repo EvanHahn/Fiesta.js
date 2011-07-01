9bf1e386f9fa723b9257f2346785260cf484ee7c
Evan Hahn <me@evanhahn.com>
2011-07-01 16:20:32 +0300

    Unit tests: misc functions (and some bug fixes)
    Fixed a bug like this:
    
      var x = new Number(5);
      Fiesta.isInteger(x);	// false! shouldn't be, isn't anymore
    
    Also changed the header to be plural

--- src/misc.js
+++ src/misc.js
@@ -1,10 +1,10 @@
-/*	Fiesta.js misc. function
+/*	Fiesta.js misc. functions
 	License info:
 	http://raw.github.com/EvanHahn/Fiesta.js/master/LICENSE.txt	*/
 
 // Check value types
 Fiesta.isNumber = function(n) { return (((typeof n === typeof 1.0) || (n instanceof Number)) && (!isNaN(n))) };
-Fiesta.isInteger = function(i) { return ((Fiesta.isNumber(i)) && (Math.floor(i) === i)) };
+Fiesta.isInteger = function(i) { return ((Fiesta.isNumber(i)) && (Math.floor(i) == i)) };
 Fiesta.isString = function(s) { return ((typeof s === typeof "") || (s instanceof String)) };
 Fiesta.isBoolean = function(b) { return ((typeof b === typeof true) || (b instanceof Boolean)) };
 Fiesta.isArray = function(a) { return a.constructor == Array };
--- tests/spec/misc_spec.js
+++ tests/spec/misc_spec.js
@@ -1,2 +1,152 @@
 describe("Misc. functions", function() {
+
+	it("checks if a primitive value is a number (when it is)", function() {
+		var x = 5;
+		expect(Fiesta.isNumber(x)).toBeTruthy();
+	});
+
+	it("checks if a primitive value is a number (when it isn't)", function() {
+		var x = "5";
+		expect(Fiesta.isNumber(x)).toBeFalsy();
+	});
+
+	it("checks if a Number object is a number (when it is)", function() {
+		var x = new Number(5);
+		expect(Fiesta.isNumber(x)).toBeTruthy();
+	});
+
+	it("checks if an object is a number (when it isn't one)", function() {
+		var x = new String("5");
+		expect(Fiesta.isNumber(x)).toBeFalsy();
+	});
+
+	it("checks if a primitive value is an integer (when it is)", function() {
+		var x = 5;
+		expect(Fiesta.isInteger(x)).toBeTruthy();
+	});
+
+	it("checks if a primitive value is an integer (when it isn't)", function() {
+		var x = 5.5;
+		expect(Fiesta.isInteger(x)).toBeFalsy();
+	});
+
+	it("checks if a Number is an integer (when it is)", function() {
+		var x = new Number(5);
+		expect(Fiesta.isInteger(x)).toBeTruthy();
+	});
+
+	it("checks if a Number is an integer (when it isn't)", function() {
+		var x = new Number(5.5);
+		expect(Fiesta.isInteger(x)).toBeFalsy();
+	});
+
+	it("checks if a non-number is an integer", function() {
+		var x = "5";
+		expect(Fiesta.isInteger(x)).toBeFalsy();
+	});
+
+	it("checks if a primitive value is a boolean (when it is)", function() {
+		var x = true;
+		expect(Fiesta.isBoolean(x)).toBeTruthy();
+	});
+
+	it("checks if a primitive value is a boolean (when it isn't)", function() {
+		var x = "true";
+		expect(Fiesta.isBoolean(x)).toBeFalsy();
+	});
+
+	it("checks if a Boolean object is a boolean (when it is)", function() {
+		var x = new Boolean(true);
+		expect(Fiesta.isBoolean(x)).toBeTruthy();
+	});
+
+	it("checks if an object is a boolean (when it isn't one)", function() {
+		var x = new String("true");
+		expect(Fiesta.isBoolean(x)).toBeFalsy();
+	});
+	
+	it("checks if a primitive array is an array (when it is)", function() {
+		var x = [1, 2, 3];
+		expect(Fiesta.isArray(x)).toBeTruthy();
+	});
+
+	it("checks if a primitive array is an array (when it isn't)", function() {
+		var x = "123";
+		expect(Fiesta.isArray(x)).toBeFalsy();
+	});
+
+	it("checks if an Array object is an array (when it is)", function() {
+		var x = new Array(1, 2, 3);
+		expect(Fiesta.isArray(x)).toBeTruthy();
+	});
+
+	it("checks if an object is an array (when it isn't one)", function() {
+		var x = new String("123");
+		expect(Fiesta.isArray(x)).toBeFalsy();
+	});
+	
+	it("checks if something is undefined (when it is)", function() {
+		var x;
+		expect(Fiesta.isUndefined(x)).toBeTruthy();
+	});
+
+	it("checks if something is undefined (when it is), even if undefined is re-defined", function() {
+		undefined = 1;
+		var x;
+		expect(Fiesta.isUndefined(x)).toBeTruthy();
+		undefined = void(0);
+	});
+	
+	it("checks if something is undefined (when it isn't)", function() {
+		var x = "undefined";
+		expect(Fiesta.isUndefined(x)).toBeFalsy();
+	});
+	
+	it("creates undefined", function() {
+		undefined = 1;
+		var x;
+		expect(Fiesta.makeUndefined()).toEqual(x);
+		undefined = x;
+	});
+	
+	it("tells you if a string contains another (if it's there)", function() {
+		var a = "Hello world!";
+		var b = "world";
+		expect(Fiesta.contains(a, b)).toBeTruthy();
+	});
+
+	it("tells you if a string contains another (if it's not there)", function() {
+		var a = "Hello world!";
+		var b = "cake";
+		expect(Fiesta.contains(a, b)).toBeFalsy();
+	});
+
+	it("tells you if an array contains a value (if it's there)", function() {
+		var a = [1, 2, 3];
+		var b = 2;
+		expect(Fiesta.contains(a, b)).toBeTruthy();
+	});
+
+	it("tells you if an array contains a value (if it's not there)", function() {
+		var a = [1, 2, 3];
+		var b = 4;
+		expect(Fiesta.contains(a, b)).toBeFalsy();
+	});
+
+	it("gets the file extension from a simple filename (when there is one)", function() {
+		expect(Fiesta.getFileExtension("hello.ogg").toEqual("ogg");
+	});
+
+	it("gets the file extension from a weird-caps filename (when there is one)", function() {
+		expect(Fiesta.getFileExtension("hello.OgG").toEqual("ogg");
+	});
+
+	it("gets the file extension from a more complex filename (when there is one)", function() {
+		expect(Fiesta.getFileExtension("hello. world..ogg").toEqual("ogg");
+	});
+
+	it("gets no file extension from a filename when there isn't one", function() {
+		expect(Fiesta.getFileExtension("hello").toEqual("");
+	});
+
 });
