5697ce9f5763a8db76f814410ec38796256e7c40
Evan Hahn <me@evanhahn.com>
2011-07-06 18:16:31 +0300

    Update combine shell script - should now combine things in right order

--- utils/combine.sh
+++ utils/combine.sh
@@ -3,4 +3,4 @@
 # This makes a combined Fiesta.js file. Still needs minification.
 
 cd ../src/
-cat namespace.js jsclass_core.js three_fiesta.js stats.js config.js misc.js browser.js commands.js math.js baseobject.js entity.js locatable.js physical.js graphic.js graphic2d.js graphic3d.js sprite2d.js box3d.js camera3d.js playground.js > ../build/fiesta_combined.js
+cat namespace.js jsclass_core.js three_fiesta.js stats.js config.js misc.js vector3.js math.js browser.js commands.js baseobject.js entity.js graphic.js graphic2d.js graphic3d.js sprite2d.js box3d.js camera3d.js light3d.js playground.js > ../build/fiesta_combined.js
