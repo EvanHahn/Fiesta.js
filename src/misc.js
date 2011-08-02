c20a5d501bf04145629503262dfa5d5a2f579b2e
EvanHahn <me@evanhahn.com>
2011-08-02 12:35:08 -0700

    Import JSTypes (well, a variant)

--- src/misc.js
+++ src/misc.js
@@ -1,19 +1,26 @@
 /*	Fiesta.js misc. functions
 	License info:
-	http://raw.github.com/EvanHahn/Fiesta.js/master/LICENSE.txt	*/
+	http://raw.github.com/EvanHahn/Fiesta.js/master/LICENSE.txt
+
+	Much of this is taken from the free license JSTypes.
+	http://github.com/EvanHahn/JSTypes
+	*/
 
 // Check value types
-Fiesta.isNumber = function(n) { return (((typeof n === typeof 1.0) || (n instanceof Number)) && (!isNaN(n))) };
-Fiesta.isInteger = function(i) { return ((Fiesta.isNumber(i)) && (Math.floor(i) == i)) };
-Fiesta.isString = function(s) { return ((typeof s === typeof "") || (s instanceof String)) };
-Fiesta.isBoolean = function(b) { return ((typeof b === typeof true) || (b instanceof Boolean)) };
-Fiesta.isArray = function(a) { return a instanceof Array };
-Fiesta.isUndefined = function(u) { return typeof u === "undefined"; };
-// If you're looking for it, isNaN() is built into JavaScript
+Fiesta.getClass = {}.toString;
+Fiesta.isNumber = function(n) { return Fiesta.getClass.call(n) == '[object Number]' };
+Fiesta.isInteger = function(i) { return Fiesta.isNumber(i) && Math.floor(i) == i };
+Fiesta.isString = function(s) { return Fiesta.getClass.call(s) == '[object String]' };
+Fiesta.isBoolean = function(b) { return Fiesta.getClass.call(b) == '[object Boolean]' };
+Fiesta.isArray = function(a) { return Fiesta.getClass.call(a) == '[object Array]' };
+Fiesta.isUndefined = function(u) { return typeof u == 'undefined' };
+Fiesta.isDefined = function(d) { return typeof d != 'undefined' };
+Fiesta.isNAN = function(i) { return i != i };
 
-// "Create" undefined (nicer-looking version of void())
-Fiesta.makeUndefined = function() { return; };
-Fiesta.makeNaN = function() { return parseInt(void(0)) };
+// Create values that could be redefined
+Fiesta.makeUndefined = function() { return };
+Fiesta.makeNaN = function() { return 0 / 0 };
+Fiesta.makeInfinity = function() { return 1 / 0 };
 
 // Does this string/array contain this element?
 Fiesta.contains = function(searchIn, searchFor) { return !!~searchIn.indexOf(searchFor) };
